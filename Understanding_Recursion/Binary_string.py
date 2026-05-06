5de80be2877b24dad7821a87a0ac2bd2ba592ce0
Okpala76 <okpalaogochukwu76@gmail.com>
2026-05-06 16:27:35 +0100

    my implementation before AI check

---
+++ Understanding_Recursion/Binary_string.py
@@ -0,0 +1,28 @@
+###the brute force is also not possible we cant tell number of iterations before hand
+#
+# hence  brute force attempt , i thing fo base case first and that would be a situation wher ethe current list has same lent as the n numer  at that point we know that list is complete
+#
+#  and anything asides that we would loop through until our while increasing curent value till we get  current value over the 1 size
+#
+
+
+def binary_possiblities(n: int):
+    result = []
+
+    def backtracking(index: int, current_list: list):
+        if len(current_list) == n:
+            result.append(current_list) 
+            return
+        current_value = 0
+        while current_value < 2:
+            current_list.append(current_value)
+            backtracking(index + 1, current_list)
+            current_list.pop()
+            current_value += 1
+
+    backtracking(0, [])
+
+    return result
+
+
+print(binary_possiblities(2))
