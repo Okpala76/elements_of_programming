6bd9ff3413c92a06b8bfdc8f986519625104bb6f
Okpala76 <okpalaogochukwu76@gmail.com>
2026-05-23 13:24:57 +0100

    I cleaned u some changes i made when recording the series video

--- Understanding_Recursion/Count_occurance.py
+++ Understanding_Recursion/Count_occurance.py
@@ -1,6 +1,9 @@
-## The thinking behind this is that i have seen it already so many times because we are simply going to go through each index recursivly and the have a counter hold the amout of times we see a value
-## this is going to be O(n) for both time and space
-## But i cant shake the feeling of an optimization on the horizon (it is for the itrative implementation)
+# The thinking behind this is
+# that i have seen it already so many times because we are simply going
+# to go through each index recursivly and the have a counter hold the
+# amout of times we see a value
+# this is going to be O(n) for both time and space
+# But i cant shake the feeling of an optimization on the horizon (it is for the iterative implementation)
 
 
 def count_occurance(string: str, value: str, count: int = 0, index: int = 0):
@@ -31,3 +34,10 @@ def iterative_count_occurance(string: str, value: str):
 
 
 print("this is the iterative version", iterative_count_occurance("banana", "a"))
+
+
+## Now one might ask what is the real advantage?
+# Very alient if you ask me because you dont see it you reason it
+# every recursive call creates an instance of the value or space holder isn every recursive call
+# this will make a seemly O(1) an O(n)
+# so yes my intuition was right on this one
--- Understanding_Recursion/Generate_all_permutation.py
+++ Understanding_Recursion/Generate_all_permutation.py
@@ -1,19 +1,25 @@
-## This solution had me look at the three understand of recursu=ion that opened me up to the understading of recursion and how it helps me cook... so unlike other solution where i had see the concepts before this was diff coz i thought it through using the recursion methd i had learnt
-## i.e is base case and recursion case
-## the base case was ease a bit and it also informed me how the recursion case will look.. what do i mean?
-## i new that the base case will be when the current list becomes the len of the arr so an append to the result will have to happen
-## and the  this lead me to the undersatnding that a back track will be required hence the pop on every call
-## then the recursion state which is straight forward
+# This solution had me look at the three understanding of recursion
+#  that opened me up to the understading of recursion and how it helps me cook...
+#  so unlike other solution where I had seen the concepts before this was diff coz
+#  I thought it through using the recursion method i had learnt
+# i.e is base case and recursion case
+# the base case was ease a bit and it also informed me how the recursion case will look.. what do i mean?
+# I knew that the base case will be when the current list becomes
+# the len of the arr so an append to the result will have to happen
+# and this lead me to the undersatnding that a back track will be
+#  required hence the pop on every call
+# then the recursion state which is straight forward
 
 
 def generate_all_permutation(arr: list):
-    result = []
+    result: list[list] = []
 
     def backtracking(index: int, current_list: list):
         # base case
         if len(arr) == index:
             return result.append(current_list.copy())
 
+        ## recursive case
         for i in range(len(arr)):
             if arr[i] not in current_list:
                 current_list.append(arr[i])
@@ -24,4 +30,36 @@ def generate_all_permutation(arr: list):
     return result
 
 
-print(generate_all_permutation(["a", "b", "c"]))
+print(generate_all_permutation(["a", "b", "c", "d"]))
+
+
+def generate_all_permutation_optimization(n: list):
+    result: list[list] = []
+    used: list = [False] * len(n)
+
+    def backtracking(index: int, current_list: list):
+        ## base case
+        if index == len(n):
+            result.append(current_list.copy())
+            return
+
+        ## recursive case
+        for i in range(len(n)):
+            if used[i]:
+                continue
+
+            used[i] = True
+
+            current_list.append(n[i])
+
+            backtracking(index + 1, current_list)
+
+            current_list.pop()
+
+            used[i] == False
+
+
+# for loop → O(n) --> O(n)
+# check → O(n) --> O(1)
+
+# Total → O(n^2) --> O(n)
